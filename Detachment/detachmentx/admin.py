ce4b2c3894749116fc7160108b7ab1efc3e25e11
yuriun <>
2020-05-11 23:13:29 -0300

    Adicinonado produtos e usuarios

--- Detachment/detachmentx/admin.py
+++ Detachment/detachmentx/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product
+from .models import User
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Product, ProductAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'name')
+
+
+admin.site.register(User, UserAdmin)
--- Detachment/detachmentx/models.py
+++ Detachment/detachmentx/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('name', max_length=100)
+    price = models.DecimalField('price', decimal_places=2, max_digits=6)
+    stock = models.IntegerField('stock amount')
+
+class User(models.Model):
+    name = models.CharField('name', max_length=110)
