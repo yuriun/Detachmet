733b556ad961b5a87f180f9639c8d5d1ee539fcf
yuriun <>
2020-05-11 23:37:45 -0300

    Apresentando dados no templates

--- Detachment/detachmentx/views.py
+++ Detachment/detachmentx/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from detachmentx.models import Product
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    listP = Product.objects.all()
+    context = {
+        'produtos':listP
+    }
+    return render(request, 'index.html', context)
 
 def contact(request):
     return render(request, 'contact.html')
